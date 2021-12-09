f9fc51d5770fa5a517a882904e63e8416c80edfb
Averagegithubusername <s0mething_different@mail.ru>
2021-12-10 01:48:59 +0300

    correction according to remarks

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,33 +1,23 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
-
-    @Test
-    void shouldReturnNumber() {
-        SQRService SQRService = new SQRService();
-        int bottomLimit = 100;
-        int topLimit = 300;
-        int expected = 8;
-        int actual = SQRService.sqrCounter(bottomLimit, topLimit);
-        assertEquals(expected, actual);
-    }
 
     @ParameterizedTest
     @CsvSource(value = {"0, 0, 0",
             "100, 100, 1",
             "9_801, 9_801, 1",
             "10_000, 10_000, 0",
+            "100, 300, 8",
             "200, 300, 3",
             "300, 200, 0"})
-     void shouldReturnNumber(int bottomLimit, int topLimit, int expected) {
-        SQRService SQRService = new SQRService();
-        int actual = SQRService.sqrCounter(bottomLimit, topLimit);
+    void shouldReturnNumber(int bottomLimit, int topLimit, int expected) {
+        SQRService sqrService = new SQRService();
+        int actual = sqrService.sqrCounter(bottomLimit, topLimit);
         assertEquals(expected, actual);
     }
 }
